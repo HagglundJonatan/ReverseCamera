4416229a6c2a50d7af90d857a197dd165a82b3b9
Jonatan <hagglund.jonatan@gmail.com>
2019-01-29 01:03:16 +0100

    Started with a way to with a button shutdown and then turn on the pi but the lcd screen complicated things.

--- VideoScripts/videostart.py
+++ VideoScripts/videostart.py
@@ -2,6 +2,10 @@ import subprocess
 
 print "Starting video stream"
 
-subprocess.call("/home/pi/Scripts/video.sh", shell=True)
+subprocess.call("/home/pi/Desktop/reverse_camera/VideoScripts/video.sh", shell=True)
+
+# Not needed right now. For a shutdown to work the lcd 
+# display must be terminated somehow, probably by wire.
+#subprocess.call("/home/pi/Desktop/reverse_camera/listen_for_shutdown.py", shell=False)
 
 print "Exiting start script"
